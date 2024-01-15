93d990ba2a0f221201ba3c721662eefbc5e76867
Gustavo Rizzo S M de Albuquerque <grizzo.albu@gmail.com>
2024-01-14 23:36:40 -0300

    Adding Postres

--- Django/core/settings.py
+++ Django/core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-n)h7y^%$$s4h2(b=7x34%t%dmhge*5mx0q72y9woa$b7tuk&ku
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '::1']
 
 
 # Application definition
@@ -75,14 +75,22 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DATABASE_NAME", "invest_track_development"),
+        "HOST": os.getenv("DATABASE_HOST", "postgres"),
+        "PORT": os.getenv("DATABASE_PORT", 5432),
+        "USER": os.getenv("DATABASE_USERNAME", "invest_track_user_development"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD", "invest_track_password"),
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
