4d9d2fd920359591a819ae36815c0a32db3486b9
Gustavo Rizzo S M de Albuquerque <grizzo.albu@gmail.com>
2024-01-29 01:35:48 -0300

    Creating yahoo finances API tests

---
+++ Django/yahoo_finances/tests/test_api.py
@@ -0,0 +1,35 @@
+
+from datetime import datetime
+from django.test import TestCase
+
+
+from ..api import fetch_and_save_company_data, fetch_company_data, convert_epoch_to_datetime
+from ..models import Company
+
+
+class BTest(TestCase):
+
+    def test_convert_epoch_to_datetime(self):
+        # Arrange
+        petr4_dict = {
+            'firstTradeDateEpochUtc': 946900800,
+            'timeZoneFullName': 'America/Sao_Paulo',
+        }
+        epoch_utc = petr4_dict['firstTradeDateEpochUtc']
+        timezone_name = petr4_dict['timeZoneFullName']
+        # Act
+        date = convert_epoch_to_datetime(epoch_utc, timezone_name)
+        # Assert
+        self.assertIsInstance(date, datetime)
+        self.assertEqual(date.year, 2000)
+        self.assertEqual(date.month, 1)
+        self.assertEqual(date.day, 3)
+
+    def test_fetch_and_save_company_data_real_touch_api(self):
+        # Arrange
+        symbol = 'PETR4.SA'
+        # Act
+        company = fetch_and_save_company_data(symbol)
+        # Assert
+        self.assertIsInstance(company, Company)
+        self.assertEqual(company.symbol, symbol)
