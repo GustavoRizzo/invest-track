eb6559d3b679d2dd33081110d2e5f4ac9b73a808
Gustavo Rizzo S M de Albuquerque <grizzo.albu@gmail.com>
2024-01-29 02:51:28 -0300

    Creating API fetch_stock_history

--- Django/yahoo_finances/api.py
+++ Django/yahoo_finances/api.py
@@ -1,12 +1,13 @@
 import logging
 import yfinance as yf
+import pandas as pd
 
 from datetime import datetime
 from pytz import timezone
 from django.core.exceptions import ValidationError
 from django.db import IntegrityError
 
-from .models import Company
+from .models import Company, DailyStockHistory
 
 
 logger = logging.getLogger(__name__)
@@ -75,3 +76,42 @@ def fetch_and_save_company_data(symbol: str) -> Company:
     # Save
     company.save()
     return company
+
+
+def convert_df_yahoo_stock_history_to_DailyStockHistory(symbol: str, data: pd.DataFrame) -> list[DailyStockHistory]:
+    assert symbol is not None, "Symbol is required"
+    assert not data.empty, "DataFrame is empty"
+
+    # List to store DailyStockHistory objects
+    daily_stock_history_list = []
+
+    # Iterate over DataFrame rows
+    for index, row in data.iterrows():
+        daily_stock_history = DailyStockHistory(
+            symbol=symbol,
+            date=index.date(),
+            open=row['Open'],
+            high=row['High'],
+            low=row['Low'],
+            close=row['Close'],
+            volume=row['Volume'],
+            dividends=row['Dividends'],
+            splits=row['Stock Splits'],
+            load_date=datetime.now()
+        )
+        # Add the object to the list
+        daily_stock_history_list.append(daily_stock_history)
+
+    return daily_stock_history_list
+
+
+def fetch_stock_history(symbol: str, start: datetime, end: datetime = datetime.now()) -> list[DailyStockHistory]:
+    # Fetch data from Yahoo Finance
+    ticker = yf.Ticker(symbol)
+    # Get the symbol from the ticker
+    symbol = ticker.info["symbol"]
+    # Get the history from the ticker
+    logger.info(f"Fetching {symbol} history from {start} to {end}")
+    data = ticker.history(start=start, end=end)
+    logger.info(f"History fetched with {len(data)} rows")
+    return convert_df_yahoo_stock_history_to_DailyStockHistory(symbol, data)
