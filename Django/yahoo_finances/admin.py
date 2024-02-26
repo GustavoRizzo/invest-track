bf7c39c7d5bc95636e81ee514d2f26c051be27fe
Gustavo Rizzo S M de Albuquerque <grizzo.albu@gmail.com>
2024-02-26 01:13:29 -0300

    Creating DailyStockHistoryAdmin

--- Django/yahoo_finances/admin.py
+++ Django/yahoo_finances/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from yahoo_finances.models import Company
+from yahoo_finances.models import Company, DailyStockHistory
+
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
@@ -9,3 +10,12 @@ class CompanyAdmin(admin.ModelAdmin):
     list_filter = ('sector', 'industry', 'exchange', 'quote_type', 'currency', 'financial_currency', 'load_date')
     date_hierarchy = 'load_date'
     ordering = ('symbol', 'short_name', 'long_name', 'sector', 'industry', 'exchange', 'quote_type', 'currency', 'financial_currency', 'load_date')
+
+
+@admin.register(DailyStockHistory)
+class DailyStockHistoryAdmin(admin.ModelAdmin):
+    list_display = ('symbol', 'date', 'open', 'high', 'low', 'close', 'volume', 'dividends', 'splits', 'load_date')
+    search_fields = ('symbol', 'date', 'open', 'high', 'low', 'close', 'volume', 'dividends', 'splits', 'load_date')
+    list_filter = ('symbol', 'date', 'open', 'high', 'low', 'close', 'volume', 'dividends', 'splits', 'load_date')
+    date_hierarchy = 'date'
+    ordering = ('symbol', 'date', 'open', 'high', 'low', 'close', 'volume', 'dividends', 'splits', 'load_date')
