d8b5c3ea9c261f904cd45191ea1216bb2a2cf764
Gustavo Rizzo S M de Albuquerque <grizzo.albu@gmail.com>
2024-02-26 01:09:33 -0300

    Creating CompanyAdmin

--- Django/yahoo_finances/admin.py
+++ Django/yahoo_finances/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from yahoo_finances.models import Company
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('symbol', 'short_name', 'long_name', 'sector', 'industry', 'exchange', 'quote_type', 'currency', 'financial_currency', 'load_date')
+    search_fields = ('symbol', 'short_name', 'long_name', 'sector', 'industry', 'exchange', 'quote_type', 'currency', 'financial_currency', 'load_date')
+    list_filter = ('sector', 'industry', 'exchange', 'quote_type', 'currency', 'financial_currency', 'load_date')
+    date_hierarchy = 'load_date'
+    ordering = ('symbol', 'short_name', 'long_name', 'sector', 'industry', 'exchange', 'quote_type', 'currency', 'financial_currency', 'load_date')
