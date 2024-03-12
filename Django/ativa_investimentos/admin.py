dccbefceeae0ffae8765595f1c72d09aa754e224
Gustavo Rizzo S M de Albuquerque <grizzo.albu@gmail.com>
2024-03-11 22:37:27 -0300

    Creating StockPositionAdmin

--- Django/ativa_investimentos/admin.py
+++ Django/ativa_investimentos/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import StockPosition
+
+
+@admin.register(StockPosition)
+class StockPositionAdmin(admin.ModelAdmin):
+    list_display = ("symbol", "quantity", "created_at", "updated_at", "is_active", "is_deleted")
+    list_filter = ("symbol", "is_active", "is_deleted")
+    search_fields = ("symbol",)
+    readonly_fields = ("created_at", "updated_at", "log_history")
+    fieldsets = (
+        (None, {
+            "fields": ("symbol", "quantity")
+        }),
+        ("Status", {
+            "fields": ("is_active", "is_deleted")
+        }),
+        ("History", {
+            "fields": ("created_at", "updated_at", "log_history"),
+            "classes": ("collapse",)
+        })
+    )
