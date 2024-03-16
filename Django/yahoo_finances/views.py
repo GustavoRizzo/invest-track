a0c58b77eb22309de6cb221eba3e4bc818b8a3e7
Gustavo Rizzo S M de Albuquerque <grizzo.albu@gmail.com>
2024-03-16 11:19:32 -0300

    Clean Code

--- Django/yahoo_finances/views.py
+++ Django/yahoo_finances/views.py
@@ -22,16 +22,12 @@ def multiple_companies(request):
 
 
 def multiple_normalized_companies(request):
-    print('\n\n ----------------- multiple_normalized_companies ----------------- ')
     # Symbols
     symbols = ['VALE3.SA', 'ASAI3.SA', 'PETR4.SA', 'MGLU3.SA']
     # Get DailyStockHistory for each symbol
     stocks = []
     for symbol in symbols:
-        print(f"symbol: {symbol}")
         stock_history = DailyStockHistory.objects.get_normalized_close('2024-01-01', symbol)
         test = stock_history.first()
-        print(f"-----------------> test: {test} , {test.normalized_close} , {test.date}")
-        # print(f"-----------------> stock_history: {stock_history}")
         stocks.append({'symbol': symbol, 'stock_history': stock_history})
     return render(request, 'multiple_normalized_companies.html', {'stocks': stocks})
