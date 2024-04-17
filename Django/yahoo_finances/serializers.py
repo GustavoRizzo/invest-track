1e7a2151f889403958038e01873d96c4821e24a0
Gustavo Rizzo S M de Albuquerque <grizzo.albu@gmail.com>
2024-04-16 23:41:17 -0300

    Adding endpoint DailyStockHistoryViewSet.Normalized

--- Django/yahoo_finances/serializers.py
+++ Django/yahoo_finances/serializers.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from django.utils import timezone
 from rest_framework import serializers
 
 from .models import DailyStockHistory
@@ -7,3 +9,16 @@ class DailyStockHistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = DailyStockHistory
         fields = '__all__'
+
+
+class InputsNormalizedCloseSerializer(serializers.Serializer):
+    start_date = serializers.DateField(default=(datetime.today().date() - timezone.timedelta(days=30)))
+    symbol = serializers.CharField(default='VALE3.SA')
+
+
+class NormalizedCloseSerializer(serializers.ModelSerializer):
+    normalized_close = serializers.FloatField()
+
+    class Meta:
+        model = DailyStockHistory
+        fields = ['symbol', 'date', 'close', 'normalized_close']
--- Django/yahoo_finances/views.py
+++ Django/yahoo_finances/views.py
@@ -1,16 +1,34 @@
 from django.shortcuts import render, get_object_or_404
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED, HTTP_200_OK
 
 from .models import Company, DailyStockHistory
-from .serializers import DailyStockHistorySerializer
+from .serializers import DailyStockHistorySerializer, InputsNormalizedCloseSerializer, NormalizedCloseSerializer
 
 
 class DailyStockHistoryViewSet(ModelViewSet):
     queryset = DailyStockHistory.objects.all()
     serializer_class = DailyStockHistorySerializer
     permission_classes = [IsAuthenticated]
-    http_method_names = ['get']
+    http_method_names = ['get', 'post',]
+
+    @swagger_auto_schema(request_body=InputsNormalizedCloseSerializer, responses={200: NormalizedCloseSerializer})
+    @action(detail=False, methods=['post'], url_path='normalized', url_name='normalized',
+            permission_classes=[IsAuthenticated])
+    def get_normalized_close(self, request):
+        start_date = request.data.get('start_date')
+        symbol = request.data.get('symbol')
+        # Validate the input
+        serializer_input = InputsNormalizedCloseSerializer(data={'start_date': start_date, 'symbol': symbol})
+        serializer_input.is_valid(raise_exception=True)
+        # Get the normalized close price
+        stock_history = DailyStockHistory.objects.get_normalized_close(start_date, symbol)
+        serializer_output = NormalizedCloseSerializer(stock_history, many=True)
+        return Response(serializer_output.data, status=HTTP_200_OK)
 
 
 def company_details(request, company_id):
