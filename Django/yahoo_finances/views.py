71969eb4d385cde243746e64c0d755e763d4873b
Gustavo Rizzo S M de Albuquerque <grizzo.albu@gmail.com>
2024-07-01 20:44:32 -0300

    Requering Authentication on CompanyViewSet

--- Django/yahoo_finances/views.py
+++ Django/yahoo_finances/views.py
@@ -87,7 +87,7 @@ def multiple_normalized_companies(request):
 class CompanyViewSet(ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
     http_method_names = ['get']
 
 
